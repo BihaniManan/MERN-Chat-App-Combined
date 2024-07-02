c969116d444f31a3f11235b14460fa8d3a064013
unknown <mananbihani02@gmail.com>
2024-07-02 12:17:07 +0530

    Eleventh Commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-//import reportWebVitals from "./reportWebVitals";
-import { ChakraProvider } from "@chakra-ui/react";
-import ChatProvider from "./Context/ChatProvider";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Use ReactDOM.createRoot for React 18
+import './index.css';
+import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
+import ChatProvider from './Context/ChatProvider';
+import { BrowserRouter } from 'react-router-dom';
 
-// Create a root element
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// Consider using a performance monitoring library like React DevTools Profiler
+// for in-depth analysis (https://reactjs.org/blog/2018/09/devtools-profiling.html)
 
-// Render the App component
+const root = ReactDOM.createRoot(document.getElementById('root')); // React 18 approach
+
 root.render(
   <ChakraProvider>
     <BrowserRouter>
