84f8ba04121c9c5b87a893403635a5ae43583d59
BihaniManan <134502916+BihaniManan@users.noreply.github.com>
2024-07-02 23:16:28 +0530

    Update Login.js

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -41,7 +41,7 @@ const Login = () => {
       };
 
       const { data } = await axios.post(
-        "https://quik-chat-backend.onrender.com/api/user/login",
+        "/api/user/login",
         { email, password },
         config
       );
