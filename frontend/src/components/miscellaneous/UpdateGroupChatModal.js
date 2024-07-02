9f6006429e127710493f81c15c82cf86123c57ef
BihaniManan <134502916+BihaniManan@users.noreply.github.com>
2024-07-02 23:20:18 +0530

    Update UpdateGroupChatModal.js

--- frontend/src/components/miscellaneous/UpdateGroupChatModal.js
+++ frontend/src/components/miscellaneous/UpdateGroupChatModal.js
@@ -46,7 +46,7 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
           Authorization: `Bearer ${user.token}`,
         },
       };
-      const { data } = await axios.get(`https://quik-chat-backend.onrender.com/api/user?search=${search}`, config);
+      const { data } = await axios.get(`/api/user?search=${search}`, config);
       console.log(data);
       setLoading(false);
       setSearchResult(data);
@@ -74,7 +74,7 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
         },
       };
       const { data } = await axios.put(
-        `https://quik-chat-backend.onrender.com/api/chat/rename`,
+        `/api/chat/rename`,
         {
           chatId: selectedChat._id,
           chatName: groupChatName,
